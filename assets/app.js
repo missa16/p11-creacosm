f17da3b1dfd5bd44834ac99fcd9f28b82fe23110
Catrais sophie <sophie.catrais@etu.univ-orleans.fr>
2023-03-19 21:34:05 +0100

    modification de la page connexion

--- assets/app.js
+++ assets/app.js
@@ -8,6 +8,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+import './css/connexion.css';
 
 // start the Stimulus application
 import './bootstrap';
