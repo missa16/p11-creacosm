06516f7c5f9fa568007fdbfa068f6d0b5ae7b769
Catrais sophie <sophie.catrais@etu.univ-orleans.fr>
2023-03-28 12:50:25 +0200

    modif

--- assets/js/historique.js
+++ assets/js/historique.js
@@ -2,26 +2,30 @@
 // recuperation des elements de la liste de sondage
 
 let displayQ = document.getElementById('displayQuestion');
-
 let listeSondages = document.querySelectorAll('.sondageDone');
 let reponses = document.querySelector('.reponses');
-listeSondages.forEach(li => {
-        let name = li.querySelector('a');
-        displayQ.style.display="none";
-        name.addEventListener("click", (event) => {
 
-                event.preventDefault();
-                // Hide all "reponses" blocks except the one associated with the clicked "li"
-                listeSondages.forEach(s => {
-                        if (s !== li) {
-                                displayQ.style.display="block";
-                                s.querySelector('.reponses').style.display = 'none';
-                        }
-                });
-                // Toggle the display property of the "reponses" block associated with the clicked "li"
-                reponses.style.display = reponses.style.display === 'none' ? 'block' : 'none';
-                displayQ.style.display= displayQ.style.display === 'none' ? 'block' : 'none';
+
+
+    listeSondages.forEach(l=>{
+        l.addEventListener('click',(event)=> {
+            event.preventDefault();
+        // Hide all "reponses" blocks except the one associated with the clicked "li"
+        listeSondages.forEach(s => {
+                reponses.innerHTML=s.dataset.reponses;
         });
-});
+    });
+        // Toggle the display property of the "reponses" block associated with the clicked "li"
+
+
+    //let target = event.target;
+    // if (target.tagName === 'A'){
+    //     event.target.classList.toggle('select');
+    //     displayQ.innerHTML="hello";
+    // }
+})
+
+
+
 
 
